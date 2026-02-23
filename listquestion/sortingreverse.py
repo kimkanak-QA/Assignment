a23cc9052bd53c1c97d2d9fd6da175656a979698
Kanak Kumari <kim.kanak@hotmail.com>
2026-02-24 03:05:29 +0530

    sorting & reverse

---
+++ listquestion/sortingreverse.py
@@ -0,0 +1,8 @@
+# Sorting & Reversing
+# Create a list of 6 random numbers, print it sorted in ascending order then descending. Don't modify the original list.
+
+num = [34,21,76,46,55,90]
+ascend = sorted(num)
+print(ascend)   # ascending
+descend = sorted(num,reverse=True)
+print(descend) #descending
