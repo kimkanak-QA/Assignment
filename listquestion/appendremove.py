eba666a91384e8764aad1a6a533c2bbae8497bee
Kanak Kumari <kim.kanak@hotmail.com>
2026-02-24 02:32:25 +0530

    Added list create & access program

---
+++ listquestion/appendremove.py
@@ -0,0 +1,14 @@
+#Append & Remove
+#Start with an empty list. Add the numbers 1–5 using .append(), then remove the number 3 and print the final list.
+
+
+num = []   # empty list
+print(num)
+num.append(1)    #append 1 to 5
+num.append(2)
+num.append(3)
+num.append(4)
+num.append(5)
+print(num)
+num.pop(3)      #delete 3
+print(num)
