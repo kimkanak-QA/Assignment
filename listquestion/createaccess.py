e90df5e9da4464d053d93e5db8b2cca747afd145
Kanak Kumari <kim.kanak@hotmail.com>
2026-02-23 23:35:05 +0530

    Added list create & access program

---
+++ listquestion/createaccess.py
@@ -0,0 +1,6 @@
+# 1. Create & Access
+# Create a list of 5 fruits and print the second and fourth item using indexing.
+
+fruits = ["apple","Mango","orange","papaya","water melon"]
+print(fruits[1])
+print(fruits[3])
