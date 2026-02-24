1b777aa6b01b5f70bde54a6e44d2efaf016be858
Kanak Kumari <kim.kanak@hotmail.com>
2026-02-25 02:26:47 +0530

    dict_createacess

---
+++ Dictionaries/createandaccess.py
@@ -0,0 +1,11 @@
+# Create & Access
+# Create a dictionary for a book with keys title, author, year, and pages. Print a sentence summarising the book using its values.
+
+book = {
+    "tittle":"Baker's world",
+    "author":"KKC",
+    "Year":2024,
+    "Pages": 160
+}
+print(book)
+print("There is book name",book["tittle"], "whose author is", book["author"], "and it was written in year",book["Year"],"and has total",book["Pages"],"pages")
