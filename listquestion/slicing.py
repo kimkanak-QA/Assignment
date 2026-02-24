4234b3d4d30a13c92cd9a26bab988180595b969f
Kanak Kumari <kim.kanak@hotmail.com>
2026-02-25 01:00:01 +0530

    slicing.py

---
+++ listquestion/slicing.py
@@ -0,0 +1,10 @@
+#Slicing
+#Given a list of the numbers 1–10, use slicing to print the first 3, the last 3, and the middle 4.
+
+num = [2,20,16,98,45,37,78,99,2,88]
+first_three =num[:3]
+print(first_three)
+last_three = num[-3:]
+print(last_three)
+middle_four = num[3:7]
+print(middle_four)
