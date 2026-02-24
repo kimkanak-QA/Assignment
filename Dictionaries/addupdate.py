40f140896d29269908fcee451350ba70ce89eb20
Kanak Kumari <kim.kanak@hotmail.com>
2026-02-25 02:51:05 +0530

    addupdate.py

---
+++ Dictionaries/addupdate.py
@@ -0,0 +1,14 @@
+# Add & Update
+# Start with a dictionary of 3 countries and their capitals. Add a new country and update the capital of an existing one. Print the final dictionary.
+
+world = {
+    "India":"Delhi",
+    "Japan":"Tokyo",
+    "Sri Lanka":"Colombo"
+}
+
+print(world)
+world["Italy"] = "Rome"
+print(world)
+world["India"] = "New Delhi"
+print(world)
